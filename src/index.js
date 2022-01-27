eb795b478e0373fef9b9b84bf7bf8112f26295ba
almacoriy <amazon407@gmail.com>
2022-01-27 23:42:33 +0200

    editing files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 // import filmcardTpl from './templates';
-// import film-servicse from './js'
+import MoviesApiService from './js/film-service';
+
+const moviesApiService = new MoviesApiService();
+const refs = {
+  form: document.querySelector('#search-form'),
+  filmCard: document.querySelector('.film-cards'),
+};
+
+refs.form.addEventListener('submit', onSearchForm);
+
+function onSearchForm() {
+  e.preventDefault();
+
+  moviesApiService.searchQuery = e.currentTarget.elements.searchQuery.value;
+}
+
+async function responseHandling() {
+  const { response } = await moviesApiService.fetchMovies();
+
+  console.log(response.data.results);
+}
+
+responseHandling();
--- src/js/film-service.js
+++ src/js/film-service.js
@@ -1,1 +1,44 @@
-// import axios from "axios";
+import axios from 'axios';
+
+const API_KEY = '07f6182ced5b52837ed98709b6b5879e';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+export default class MoviesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchMovies() {
+    // const params = {
+    //   key: API_KEY,
+    //   query: this.searchQuery,
+    //   page: this.page,
+    //   time_window: day,
+    //   media_type: movie,
+    // };
+
+    this.incrementPage();
+    const response = await axios('/trending/all/day?api_key=07f6182ced5b52837ed98709b6b5879e');
+
+    console.log(response);
+    return { response };
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get searchQuery() {
+    return this.searchForm;
+  }
+
+  set searchQuery(newQuery) {
+    this.searchForm = newQuery;
+  }
+}
